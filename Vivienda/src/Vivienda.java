bbce6a45eaa52ac675e5981f30d176779ca8c6f2
josan <josan@DESKTOP-CJKSAQG>
2022-03-01 12:57:22 +0100

    documentación realizada

--- Vivienda/src/Vivienda.java
+++ Vivienda/src/Vivienda.java
@@ -5,12 +5,21 @@
  */
 
 /**
- *
- * @author Paqui Elena
+ * @version 1.0
+ * @author José Antonio Guillamón Melero
+ * @since 01/03/2022
  */
 public class Vivienda 
 {
-
+    /**
+     * Crea un objeto Vivienda fijando todos sus atributos con los atributos pasados por parámetros
+     * @param precio Cantidad de euros que cuesta la vivienda
+     * @param numHabitaciones Número de habitaciones que tiene la vivienda
+     * @param superficie Número de metros cuadrados de la vivienda
+     * @param parking Indica si la vivienda cuenta con plaza de parking
+     * @param estado Texto descriptivo del estado actual de la vivienda
+     * @param propietario Nombre del propietariod de la vivienda
+     */
     public Vivienda(double precio, int numHabitaciones, double superficie, boolean parking, String estado, String propietario) {
         this.precio = precio;
         this.numHabitaciones = numHabitaciones;
@@ -19,6 +28,7 @@ public class Vivienda
         this.estado = estado;
         this.propietario = propietario;
     }
+    
     private double precio;
     private int numHabitaciones;
     private double superficie;
@@ -26,16 +36,27 @@ public class Vivienda
     private String estado;
     private String propietario;
     
+    /**
+     * Crea un objeto Vivienda fijando unicamente el atributo precio con el pasado por parámetro
+     * @param precio Cantidad de euros que cuesta la vivienda
+     */
     public Vivienda (double precio)
     {
         this.precio = precio;
     }
     
+    /**
+     * Actualiza el valor del atributo precio restandole el descuento pasado por parámetro
+     * @param descuento Cantidad a descontar
+     */
     public void actualizarPrecio(double descuento)
     {
         this.precio = precio - descuento;
     }
     
+    /**
+     * Imprimer por consola el precio, número de habitaciones, superficie en m2, disponibilidad de parking, estado y propietario de la vivienda
+     */
     public void imprimirVivienda(){
         System.out.println("Precio: " + this.precio);
         System.out.println("Número de habitaciones: "+ this.numHabitaciones);
@@ -45,50 +66,98 @@ public class Vivienda
         System.out.println("Propietario: "+ this.propietario);
     }
 
+    /**
+     * Obtiene el valor de precio
+     * @return Cantidad de euros que cuesta la vivienda
+     */
     public double getPrecio() {
         return precio;
     }
-
+    
+    /**
+     * Fija el valor de precio
+     * @param precio Cantidad de euros que cuesta la vivienda
+     */
     public void setPrecio(double precio) {
         this.precio = precio;
     }
 
+    /**
+     * Obtiene el valor de numHabitaciones
+     * @return Número de habitaciones que tiene la vivienda
+     */
     public int getNumHabitaciones() {
         return numHabitaciones;
     }
-
+    
+    /**
+     * Fija el valor de numHabitaciones
+     * @param numHabitaciones Número de habitaciones que tiene la vivienda
+     */
     public void setNumHabitaciones(int numHabitaciones) {
         this.numHabitaciones = numHabitaciones;
     }
-
+    
+    /**
+     * Obtiene el valor de superficie
+     * @return Número de metros cuadrados de la vivienda
+     */
     public double getSuperficie() {
         return superficie;
     }
 
+    /**
+     * Fija el valor de superficie
+     * @param superficie Número de metros cuadrados de la vivienda
+     */
     public void setSuperficie(double superficie) {
         this.superficie = superficie;
     }
 
+    /**
+     * Obtiene el valor de parking
+     * @return Indica si la vivienda cuenta con plaza de parking
+     */
     public boolean isParking() {
         return parking;
     }
 
+    /**
+     * Fija el valor de parking
+     * @param parking Indica si la vivienda cuenta con plaza de parking
+     */
     public void setParking(boolean parking) {
         this.parking = parking;
     }
 
+    /**
+     * Obtiene el valor de estado
+     * @return Texto descriptivo del estado actual de la vivienda
+     */
     public String getEstado() {
         return estado;
     }
 
+    /**
+     * Fija el valor de estado
+     * @param estado Texto descriptivo del estado actual de la vivienda
+     */
     public void setEstado(String estado) {
         this.estado = estado;
     }
 
+    /**
+     * Obtiene el valor de propietario
+     * @return Nombre del propietariod de la vivienda
+     */
     public String getPropietario() {
         return propietario;
     }
 
+    /**
+     * Fija el valor de propietario
+     * @param propietario Nombre del propietariod de la vivienda
+     */
     public void setPropietario(String propietario) {
         this.propietario = propietario;
     }
